45c4f1ed61aa080d3084fc39c94f7ce9dcf2cefe
Ocrezzu <davidjrf95@gmail.com>
2021-08-29 05:05:27 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/analytics";
 import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDTPMUZ7LqJRvFYVzZQedFRO_-1uIKsxTc",
+    apiKey: "",
     authDomain: "mascotas-3c0f1.firebaseapp.com",
     projectId: "mascotas-3c0f1",
     storageBucket: "mascotas-3c0f1.appspot.com",
     messagingSenderId: "640981596346",
-    appId: "1:640981596346:web:ea67c088cbc96ad2b4ea83",
+    appId: "",
     measurementId: "G-0MK49SKR6H"   
   };
 
