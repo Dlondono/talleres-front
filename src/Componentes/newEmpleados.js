e25684679c672b17028cd465b2d8b800470a0886
Ocrezzu <davidjrf95@gmail.com>
2021-09-15 19:32:36 -0500

    Admin

---
+++ src/Componentes/newEmpleados.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './contenedor.css';
+
+export default function newEmpleados (){
+    return (
+
+        <div>
+            <br></br>
+            <div class="container">
+            
+                       
+            </div>
+        </div>
+    )
+}
