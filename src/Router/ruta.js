3250bc08923131f47cddf5606a42321f017a0951
Dll <test@udea.com>
2021-09-15 19:59:12 -0500

    cambio crear doc mascotas uid

--- src/Router/ruta.js
+++ src/Router/ruta.js
@@ -3,12 +3,10 @@ import Registrar from '../Home/registro.js';
 import Home from '../Home/home.js';
 import homeP from '../Home/homeP.js';
 import User  from '../User/usuario.js';
-import administrador from '../User/administrador.js';
 import {BrowserRouter,Route} from 'react-router-dom';
 import Empleados from '../view/vistaEmpleado.js';
 import newEmpleados from '../Componentes/newEmpleados.js';
-
-
+import administrador from '../User/administrador.js';
 
 function ruta(){
     return (
